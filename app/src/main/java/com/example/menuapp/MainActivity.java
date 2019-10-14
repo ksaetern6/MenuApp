11858752f13153a84fa28e17bb2325db1c27bbf4
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-10-14 00:59:48 -0700

    imageAnalysis case added and passes bucket link to new activity

--- app/src/main/java/com/example/menuapp/MainActivity.java
+++ app/src/main/java/com/example/menuapp/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.menuapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageAnalysisConfig;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -68,15 +84,40 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        findViewById(R.id.imgCapture).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, imageDisplay.class);
-                startActivity(intent);
-            }
-        });
+        ImageAnalysisConfig iConfig = new ImageAnalysisConfig.Builder().build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis(iConfig);
 
-        CameraX.bindToLifecycle(this, preview); //imgCap
+        imageAnalysis.setAnalyzer(
+                new ImageAnalysis.Analyzer() {
+                    @Override
+                    public void analyze(ImageProxy imageProxy, int degrees) {
+                        if (imageProxy == null || imageProxy.getImage() == null) {
+                            return;
+                        }
+                        Image mediaImage = imageProxy.getImage();
+                        int rotation = degreesToFirebaseRotation(degrees);
+                        FirebaseVisionImage image = FirebaseVisionImage.fromMediaImage(mediaImage,rotation);
+                        detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+
+                        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                                    @Override
+                                    public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                                        processResult(firebaseVisionBarcodes);
+                                    }
+                                });
+                    }
+                }
+        );
+//        findViewById(R.id.imgCapture).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, imageDisplay.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        CameraX.bindToLifecycle(this, preview, imageAnalysis); //imgCap
 
     }
     /*
@@ -94,4 +135,53 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+    /*
+    @name: degreesToFirebaseRotation
+    @desc: Deals with CameraX camera rotation.
+
+    @doc: https://developer.android.com/training/permissions/requesting
+    */
+    private int degreesToFirebaseRotation(int degrees) {
+        switch (degrees) {
+            case 0:
+                return FirebaseVisionImageMetadata.ROTATION_0;
+            case 90:
+                return FirebaseVisionImageMetadata.ROTATION_90;
+            case 180:
+                return FirebaseVisionImageMetadata.ROTATION_180;
+            case 270:
+                return FirebaseVisionImageMetadata.ROTATION_270;
+            default:
+                throw new IllegalArgumentException(
+                        "Rotation must be 0, 90, 180, or 270.");
+        }
+    }
+    /*
+    @name: processResult
+    @desc: Load ImageDisplay activity with passed value of barcode to do a firebase bucket lookup.
+
+    @doc: https://developer.android.com/training/permissions/requesting
+    */
+    private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+        for(FirebaseVisionBarcode barcode: firebaseVisionBarcodes) {
+            int valueType = barcode.getValueType();
+
+            switch (valueType) {
+                case FirebaseVisionBarcode.TYPE_TEXT:
+                {
+                    String msg = barcode.getRawValue();
+//                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                    Log.d("mainActivity",msg);
+                    Intent intent = new Intent(MainActivity.this, imageDisplay.class);
+                    intent.putExtra("FBRef", msg);
+                    startActivity(intent);
+                    break;
+                }
+            }
+
+        }
+    }
 }
+
+
+
--- app/src/main/java/com/example/menuapp/imageDisplay.java
+++ app/src/main/java/com/example/menuapp/imageDisplay.java
@@ -20,26 +20,32 @@ public class imageDisplay extends AppCompatActivity {
     // allow read, write: if request.auth != null;
     FirebaseStorage storage = FirebaseStorage.getInstance();
     StorageReference storageRef = storage.getReference();
+    String bucketStorageRef = "images";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_display);
 
-        //Intent intent = getIntent();
+        Intent intent = getIntent();
+        bucketStorageRef = intent.getStringExtra("FBRef");
+        Log.d("imageAnalysis", bucketStorageRef);
         printImages();
     }
 
     private void printImages() {
         Log.d("imageDisplay","PRINTIMAGES RUNNING");
 
-        StorageReference sandwichRef = storage.getReferenceFromUrl(
-                "gs://menuapp-f3764.appspot.com/sandwich.jpg");
+//        StorageReference sandwichRef = storage.getReferenceFromUrl(
+//                "gs://menuapp-f3764.appspot.com/sandwich.jpg");
+
+        StorageReference imageRef = storageRef.child(bucketStorageRef);
+        Log.d("imageDisplay",imageRef.getPath());
+
         final long ONE_MB = 1024 * 1024;
-        sandwichRef.getBytes(ONE_MB).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+        imageRef.getBytes(ONE_MB).addOnSuccessListener(new OnSuccessListener<byte[]>() {
             @Override
             public void onSuccess(byte[] bytes) {
-                Log.d("imageDisplay","This is printing!");
                 Bitmap bmp = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
                 ImageView image = findViewById(R.id.imageView1);
 
